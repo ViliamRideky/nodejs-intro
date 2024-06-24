cff46d4d302b9765d8b7f10b75907824deeb291c
ViliamRideky <rideky.viliam@gmail.com>
2024-06-24 20:30:30 +0200

    fix for update.ts

--- src/handlers/update.ts
+++ src/handlers/update.ts
@@ -1,7 +1,7 @@
 import prisma from "../db"
 
-// Get all
-export const getUpdates = async (req, res) => {
+// Get one update
+export const getOneUpdate = async (req, res) => {
     const update = await prisma.update.findUnique({
         where: {
             id: req.params.id
@@ -11,8 +11,8 @@ export const getUpdates = async (req, res) => {
     res.json({data: update})
 }
 
-// Get one update
-export const getOneUpdate = async (req, res) => {
+// Get all
+export const getUpdates = async (req, res) => {
     const products = await prisma.product.findMany({
         where: {
             belongsToId: req.user.id
