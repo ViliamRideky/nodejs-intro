f3349b936d32fbf656e6a947b08cca8db2f7d53f
ViliamRideky <rideky.viliam@gmail.com>
2024-06-24 14:21:28 +0200

    added another controls for bearer

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -17,5 +17,22 @@ export const protect = (req, res, next) => {
         return
     }
 
-    
+    const [, token] = bearer.split('')
+
+    if(!token){
+        res.status(401)
+        res.json({message: 'Not authorized'})
+        return
+    }
+
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = user
+        next()
+    } catch(e) {
+        console.error(e)
+        res.status(401)
+        res.json({message: 'Not authorized'})
+        return
+    }
 }
