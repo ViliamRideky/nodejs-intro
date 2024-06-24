312913e25299579e20e7635ca978fd103bddcd2b
ViliamRideky <rideky.viliam@gmail.com>
2024-06-24 20:27:38 +0200

    added another methods for update

--- src/handlers/update.ts
+++ src/handlers/update.ts
@@ -29,10 +29,84 @@ export const getOneUpdate = async (req, res) => {
 }
 
 // Create a update
-export const createUpdate = async (req, res) => {}
+export const createUpdate = async (req, res) => {
+    const product = await prisma.product.findUnique({
+        where: {
+            id: req.body.id
+        }
+    })
+
+    if(!product) {
+        // does not belong to user
+        res.json({message: 'nope'})
+    }
+
+    const update = await prisma.update.create({
+        data: req.body
+    })
+
+    res.json({data: update})
+}
 
 // Update a update
-export const updateUpdate = async (req, res) => {}
+export const updateUpdate = async (req, res) => {
+    const products = await prisma.product.findMany({
+        where: {
+            belongsToId: req.user.id
+        },
+        include: {
+            updates: true
+        }
+    })
+
+    const updates = products.reduce((allUpdates, product) => {
+        return [...allUpdates, ...product.updates]
+    }, [])
+
+    const match = updates.find(update => update.id === req.params.id)
+
+    if(!match){
+        // handle this
+        res.json({message: 'nope'})
+    }
+
+    const updated = await prisma.update.update({
+        where: {
+            id: req.params.id
+        },
+        data: req.body
+    })
+
+    res.json({data: updated})
+}
 
 // Delete update
-export const deleteUpdate = async (req, res) => {}
+export const deleteUpdate = async (req, res) => {
+    const products = await prisma.product.findMany({
+        where: {
+            belongsToId: req.user.id
+        },
+        include: {
+            updates: true
+        }
+    })
+
+    const updates = products.reduce((allUpdates, product) => {
+        return [...allUpdates, ...product.updates]
+    }, [])
+
+    const match = updates.find(update => update.id === req.params.id)
+
+    if(!match){
+        // handle this
+        res.json({message: 'nope'})
+    }
+
+    const deleted = await prisma.update.delete({
+        where:  {
+            id: req.params.id
+        }
+    })
+
+    res.json({data: deleted})
+}
